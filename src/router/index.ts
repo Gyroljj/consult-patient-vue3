43cfd59ec8c65fb7cc27b506d9ef817fe0a98667
lijianjun <gyro239@outlook.com>
2024-07-15 17:22:21 +0800

    feat:实现引入vant和移动端适配

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,6 +10,6 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: []
 })
-console.log(import.meta)
+// console.log(import.meta)
 
 export default router
