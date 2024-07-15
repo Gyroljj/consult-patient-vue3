c96d07f596ef3f739bc6b1f7541d5f1f6c9f2bf7
lijianjun <gyro239@outlook.com>
2024-07-15 16:49:23 +0800

    feat:目录调整和路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
+// 如何得到路由示例 createRouter()
+// 如何设置路由模式 history
+// history 模式 createWebHistory()
+// hash 模式 createWebHashHistory()
 const router = createRouter({
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  // 默认参数 '/' 路由的基础路径
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+  routes: []
 })
+console.log(import.meta)
 
 export default router
