6af3bb037f3201ddbba7124eb2de97aa9730a5e3
lijianjun <gyro239@outlook.com>
2024-07-16 11:27:02 +0800

    feat: cp-nav-bar 功能标题和右侧文字和点击右侧文字的自定义事件

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,8 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const fn = () => {
+  console.log('注册')
+}
+</script>
 
 <template>
   <div class="login-page">
-    <cp-nav-bar></cp-nav-bar>
+    <cp-nav-bar @click-right="fn" title="登录" right-text="注册"></cp-nav-bar>
   </div>
 </template>
 
