9c1fc73e2c9019f8edf89057595d8328d855eac4
lijianjun <gyro239@outlook.com>
2024-07-16 10:44:45 +0800

    feat: 组件的自动注册

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="login-page">login</div>
+  <div class="login-page">
+    <van-button>按钮</van-button>
+    <van-checkbox>复选框</van-checkbox>
+  </div>
 </template>
 
 <style scoped lang="scss"></style>
