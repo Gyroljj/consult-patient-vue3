d65220e23a11edf0ac921fff3f5e3417c0316ba9
lijianjun <gyro239@outlook.com>
2024-07-17 10:53:33 +0800

    feat: 布局容器-tabbar切换效果

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -4,8 +4,53 @@
   <div class="layout-page">
     <!-- 二级路由出口 -->
     <router-view></router-view>
-    <div>tabbar</div>
+    <van-tabbar route>
+      <van-tabbar-item to="/home">
+        首页
+        <template #icon="{ active }">
+          <cp-icon :name="`home-index-${active ? 'active' : 'default'}`">
+          </cp-icon>
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item to="/article">
+        建康百科
+        <template #icon="{ active }">
+          <cp-icon :name="`home-article-${active ? 'active' : 'default'}`">
+          </cp-icon>
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item to="/notify">
+        消息通知
+        <template #icon="{ active }">
+          <cp-icon :name="`home-notice-${active ? 'active' : 'default'}`">
+          </cp-icon>
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item to="/user">
+        我的
+        <template #icon="{ active }">
+          <cp-icon :name="`home-mine-${active ? 'active' : 'default'}`">
+          </cp-icon>
+        </template>
+      </van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.layout-page {
+  :deep() {
+    .van-tabbar-item {
+      &__icon {
+        font-size: 21px;
+      }
+      &__text {
+        font-size: 11px;
+      }
+      &:not(.van-tabbar-item--active) {
+        color: var(--cp-text3);
+      }
+    }
+  }
+}
+</style>
