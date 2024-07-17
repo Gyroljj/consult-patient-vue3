d921449bc5c7a5db3e76b3505698714a4f2a7ee9
lijianjun <gyro239@outlook.com>
2024-07-17 21:24:07 +0800

    feat: 单选按钮组件封装

--- src/components/CpRadioBtn.vue
+++ src/components/CpRadioBtn.vue
@@ -1,3 +1,54 @@
+<script setup lang="ts">
+defineProps<{
+  options: { label: string; value: number | string }[]
+  modelValue: number | string
+}>()
+
+defineEmits<{
+  (e: 'update:modelValue', value: number | string): void
+}>()
+</script>
+
+<template>
+  <div class="cp-radio-btn">
+    <a
+      class="item"
+      href="javascript:;"
+      v-for="item in options"
+      :key="item.value"
+      :class="{ active: modelValue === item.value }"
+      @click="$emit('update:modelValue', item.value)"
+    >
+      {{ item.label }}
+    </a>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.cp-radio-btn {
+  display: flex;
+  flex-wrap: wrap;
+  .item {
+    height: 32px;
+    min-width: 60px;
+    line-height: 30px;
+    padding: 0 14px;
+    text-align: center;
+    border: 1px solid var(--cp-bg);
+    background-color: var(--cp-bg);
+    margin-right: 10px;
+    box-sizing: border-box;
+    color: var(--cp-text2);
+    margin-bottom: 10px;
+    border-radius: 4px;
+    transition: all 0.3s;
+    &.active {
+      border-color: var(--cp-primary);
+      background-color: var(--cp-plain);
+    }
+  }
+}
+</style>
 <!-- <script setup lang="ts">
 // 计数器
 // 通过 v-model 解析成 modelValue @update:modelValue
@@ -19,7 +70,7 @@ defineEmits<{
 
 <style scoped lang="scss"></style> -->
 
-<script setup lang="ts">
+<!-- <script setup lang="ts">
 // 计数器
 // 通过 v-model:count 解析成 count @update:count
 defineProps<{
@@ -38,4 +89,4 @@ defineEmits<{
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss"></style> -->
