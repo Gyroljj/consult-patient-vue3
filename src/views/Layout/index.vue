e0a9530949a08fe756505a3567ed2f277b9f8445
lijianjun <gyro239@outlook.com>
2024-07-17 10:34:17 +0800

    feat: 布局容器组件和路由规则

---
+++ src/views/Layout/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="layout-page">
+    <!-- 二级路由出口 -->
+    <router-view></router-view>
+    <div>tabbar</div>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
---
+++ src/views/Notify/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="notify-page">notify</div>
+</template>
+
+<style scoped lang="scss"></style>
