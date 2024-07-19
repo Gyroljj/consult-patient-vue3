9352618763c0a22abbf50d38b8e978efbb33f58a
lijianjun <gyro239@outlook.com>
2024-07-19 11:41:13 +0800

    feat: 极速问诊-问诊信息对象类型

--- src/enums/index.ts
+++ src/enums/index.ts
@@ -25,15 +25,37 @@
 // }
 
 // 可以是字符串，不会依次类推
-enum Direction {
-  UP = 'Up',
-  DOWN = 'Down',
-  LEFT = 'Left',
-  RIGHT = 'Right'
+// enum Direction {
+//   UP = 'Up',
+//   DOWN = 'Down',
+//   LEFT = 'Left',
+//   RIGHT = 'Right'
+// }
+
+// const changeDirection = (direction: Direction) => {
+//   console.log(direction)
+// }
+
+// changeDirection(Direction.DOWN)
+
+// 问诊类型
+export enum ConsultType {
+  /** 找医生 */
+  Doctor = 1,
+  /** 快速问诊 */
+  Fast = 2,
+  /** 开药问诊 */
+  Medication = 3
 }
 
-const changeDirection = (direction: Direction) => {
-  console.log(direction)
+// 问诊时间，以1自增可以省略
+export enum IllnessTime {
+  /** 一周内 */
+  Week = 1,
+  /** 一月内 */
+  Month,
+  /** 半年内 */
+  HalfYear,
+  /** 半年以上 */
+  More
 }
-
-changeDirection(Direction.DOWN)
