563d96f2004d3afb204528f35898d2b1d79fe14f
lijianjun <gyro239@outlook.com>
2024-07-22 21:44:52 +0800

    feat: 问诊室-文字聊天-发送文字

--- src/views/Room/index.vue
+++ src/views/Room/index.vue
@@ -65,6 +65,18 @@ onMounted(() => {
 onUnmounted(() => {
   socket.close()
 })
+
+// 发送文字信息
+const onSendText = (text: string) => {
+  socket.emit('sendChatMsg', {
+    from: store.user?.id,
+    to: consult.value?.docInfo?.id,
+    msgType: MsgType.MsgText,
+    msg: {
+      content: text
+    }
+  })
+}
 </script>
 
 <template>
@@ -83,6 +95,7 @@ onUnmounted(() => {
     ></room-message>
     <!-- 操作栏 -->
     <room-action
+      @send-text="onSendText($event)"
       :disabled="consult?.status !== OrderType.ConsultChat"
     ></room-action>
   </div>
